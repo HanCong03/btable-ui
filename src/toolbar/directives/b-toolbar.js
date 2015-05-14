3e695467150e15162a412fe67ba70fa32352efb5
hancong03 <hancong03@baidu.com>
2015-05-14 11:36:54 +0800

    增加阻止事件的默认行为
    Signed-off-by: hancong03 <hancong03@baidu.com>

---
+++ src/toolbar/directives/b-toolbar.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @author hancong03@baiud.com
+ */
+
+angular.module('app').directive('bToolbar', [function () {
+
+    return {
+        restrict: 'A',
+        scope: {
+        },
+        link: {
+            post: function ($scope, $ele, $attr, $controller) {
+                $ele.on('mousedown', function (evt) {
+                    evt.preventDefault();
+                });
+            }
+        }
+    };
+}]);
